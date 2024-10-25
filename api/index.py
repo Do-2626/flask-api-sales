564b13dc01ba50ba1e4e291c53707b6bec8dfe1d
Your Name <you@example.com>
2024-10-25 10:10:35 +0300

    send json

--- api/index.py
+++ api/index.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return 'Hello, World!'
 
+
 @app.route('/about')
 def about():
     return 'About'
+
+
+@app.route('/jsonify')
+def jsonify():
+    return jsonify({"message": "Sale not found"}), 404
